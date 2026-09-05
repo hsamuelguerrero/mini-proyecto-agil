91107a33e8762e543fc09cded1627433da0a2c9a
hsamuelguerrero <hsamuel.guerrero@gmail.com>
2026-09-04 22:06:13 -0600

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -9,6 +9,18 @@ def restar(a, b):
     return a - b
 
 
+def multiplicar(a, b):
+    return a * b
+
+
+def dividir(a, b):
+    if b == 0:
+        raise ValueError("No se puede dividir entre cero")
+    return a / b
+
+
 if __name__ == "__main__":
     print("Suma 5 + 3:", sumar(5, 3))
     print("Resta 5 - 3:", restar(5, 3))
+    print("Multiplicación 4 * 2:", multiplicar(4, 2))
+    print("División 10 / 2:", dividir(10, 2))
