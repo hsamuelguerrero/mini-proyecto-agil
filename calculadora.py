d642845ed4347ef2e1ff0f47dc4478266e7c0848
hsamuelguerrero <hsamuel.guerrero@gmail.com>
2026-09-04 22:02:07 -0600

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+"""Calculadora básica para simulación ágil."""
+
+
+def sumar(a, b):
+    return a + b
+
+
+def restar(a, b):
+    return a - b
+
+
+if __name__ == "__main__":
+    print("Suma 5 + 3:", sumar(5, 3))
+    print("Resta 5 - 3:", restar(5, 3))
